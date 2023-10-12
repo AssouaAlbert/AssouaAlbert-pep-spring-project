e58fe5d72dcdd5fa8b3e443bcd8acace1a522488
Albert E. Assoua <assouaalbert@gmail.com>
2023-10-12 05:19:43 +0000

    Opening Project

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -1,4 +1,22 @@
 package com.example.repository;
 
-public interface MessageRepository {
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.example.entity.Account;
+import com.example.entity.Message;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Integer> {
+    // List<Message> findAllByPosedBy(Integer account_id);
+    //  List<Message> findByUsername(String username);
+    // List<Message> findByPostedBy(Integer account_id);
+     @Query("SELECT e FROM Message e WHERE e.posted_by = :userId")
+    List<Message> findByPostedBy(@Param("userId") Integer  userId);
 }
